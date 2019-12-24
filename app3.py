d89cbcc9ab72a3b21fc2e064b75d930f73afe73e
H4mid2019 <hamidreza.darabi@hotmail.com>
2019-12-24 23:22:41 +0200

    messages added

--- app3.py
+++ app3.py
@@ -8,11 +8,8 @@ import threading
 app = Flask('StarshipsAPI')
 api = Api(app)
 
-
-
-
-starships_sorted = []
-starships_unknown_hyperdrive = []
+starships_sorted = [{'message':'Updating...please retry after 15 seconds.'}]
+starships_unknown_hyperdrive = [{'message':'Updating...please retry after 15 seconds.'}]
 
 def retriever():
     global starships_sorted
@@ -24,6 +21,8 @@ def retriever():
         try:
             res = requests.get(url)
         except requests.ConnectionError:
+            starships_sorted = [{'message':'There is an issue with connecting to the server.'}]
+            starships_unknown_hyperdrive = [{'message':'There is an issue with connecting to the server.'}]
             return "Connection Error"            
         ii += 1
         l.append(json.loads(res.text)['results'])
@@ -42,6 +41,7 @@ def retriever():
     return True 
 
 
+threading.Thread(name="retriever", target=retriever).start()
 
 class StartshipsRestful(Resource):
     def get(self):
@@ -51,7 +51,6 @@ class StartshipsRestful(Resource):
 api.add_resource(StartshipsRestful, '/')
 
 if __name__ == '__main__':
-    threading.Thread(name="retriever", target=retriever).start()
     app.run(debug=True, port=8082)
 
     
